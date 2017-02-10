0e0a2e8bf6dc915649b2e24f72bc3ec600e95985
ajpadilla <ajpadilla88@gmail.com>
2017-02-10 17:58:35 -0400

    Agregando componente para modelo type-property

---
+++ public/angular/app/type-property.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-type-property',
+  template: `<h3>Type property Component</h3>`
+})
+export class TypePropertyComponent implements OnInit {
+	ngOnInit(): void {
+
+	}
+}
