8bfc836441940251efd8c3302cb19a54ea0bdd59
ajpadilla <ajpadilla88@gmail.com>
2017-02-10 18:01:51 -0400

    Agregando AppRoutingModule

--- public/angular/app/app.module.ts
+++ public/angular/app/app.module.ts
@@ -1,11 +1,25 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent }  from './app.component';
+import { TypePropertyComponent } from './type-property.component';
+
+import {APP_BASE_HREF} from '@angular/common';
+
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ 
+  	BrowserModule,  
+  	AppRoutingModule
+  ],
+  declarations: [ 
+  	AppComponent,
+  	TypePropertyComponent
+  ],
+  providers: [
+    {provide: APP_BASE_HREF, useValue : '/' }
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
