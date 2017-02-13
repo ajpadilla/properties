b1caa0103eba0c6187b645b31393d2724a1fa52f
ajpadilla <ajpadilla88@gmail.com>
2017-02-13 14:43:32 -0400

    Actualizando controladro

--- app/Http/Controllers/TypePropertyController.php
+++ app/Http/Controllers/TypePropertyController.php
@@ -7,5 +7,8 @@ use App\Models\TypeProperty;
 
 class TypePropertyController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+    	return response()->json(TypeProperty::all());
+    }
 }
