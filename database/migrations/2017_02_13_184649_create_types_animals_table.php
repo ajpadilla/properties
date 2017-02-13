553fe806d26243cf043974c8db77d8f72f2bcd4d
ajpadilla <ajpadilla88@gmail.com>
2017-02-13 14:49:23 -0400

    Agregando migración para tabla tipos de animales

---
+++ database/migrations/2017_02_13_184649_create_types_animals_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTypesAnimalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('types_animals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->name('name', 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('types_animals');
+    }
+}
