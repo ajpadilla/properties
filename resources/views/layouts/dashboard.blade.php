3ad5e05e06ec1ca12191b7e7925722ebfebb0862
ajpadilla <ajpadilla88@gmail.com>
2017-02-16 15:22:34 -0400

    Actualizando dashboard

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -4,30 +4,14 @@
     <meta charset="UTF-8">
     <title>Properties</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-    <base href="/" />
-
     <!-- Styles -->
     {!! Html::style('css/all.min.css') !!}
-
-    <link rel="stylesheet" href="/angular/styles.css">
-
-    <!-- Polyfill(s) for older browsers -->
-    <script src="{{ asset('/angular/node_modules/core-js/client/shim.min.js') }}"></script>
-
-    <script src="{{ asset('/angular/node_modules/zone.js/dist/zone.js') }}"></script>
-    <script src="{{ asset('/angular/node_modules/systemjs/dist/system.src.js') }}"></script>
-
-    <script src="{{ asset('/angular/systemjs.config.js') }}"></script>
-
-    <script>
-      System.import('app').catch(function(err){ console.error(err); });
-    </script>
 
     @stack('vue-styles')
     @stack('styles')
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
-    <div class="wrapper">
+    <div class="wrapper" id="app">
         @include('layouts.header')
         @include('layouts.sidebar')
 
@@ -47,8 +31,6 @@
 
             <!-- Main content -->
             <section class="content">
-
-              <my-app>Loading AppComponent content here ...</my-app>
 
               @yield('content')
 
@@ -260,7 +242,7 @@
 
     <!-- Scripts -->
     {!! Html::script('js/all.min.js') !!}
-
+    {!! Html::script('js/crud.js') !!}
     @yield('scripts')
     @stack('vue-scripts')
 </body>
