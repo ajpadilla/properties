9f32bb05e999f9d6b788dbb5dd9b61ad3fa6c9d3
ajpadilla <ajpadilla88@gmail.com>
2017-02-17 18:32:44 -0400

    Agregando archivo para configurar campos del modelo typeProperty

---
+++ public/js/models/typeProperty/config.js
@@ -0,0 +1,41 @@
+var mapVar = 'inputMaterialsOptions'; 
+
+var objectRow = {            
+    id: "",
+    name: "",
+    created_at: "",
+    updated_at: ""
+};
+
+var tableColumns = [
+    {
+        name: 'id',
+        sortField: 'id',
+        visible: false
+    },
+    {
+        name: 'name',
+        title: 'Nombre',
+        sortField: 'name',
+        visible: true
+    },
+    {
+        name: 'created_at',
+        title: 'Creado en',
+        sortField: 'created_at',
+        visible: false
+    },
+    {
+        name: 'updated_at',
+        title: 'Actualizado en',
+        sortField: 'updated_at',
+        visible: false
+    },       
+    {
+        name: '__component:custom-actions',
+        title: 'Actions',
+        titleClass: 'text-center',
+        dataClass: 'text-center'
+    }       
+];
+
