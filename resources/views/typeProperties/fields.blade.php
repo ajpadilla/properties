709b7913dfbc63aa4b2538023e0e9e6db833d37d
ajpadilla <ajpadilla88@gmail.com>
2017-02-20 17:10:28 -0400

    Agregando blade para inputs del formulario crear/editar Type Property

---
+++ resources/views/typeProperties/fields.blade.php
@@ -0,0 +1,24 @@
+<div class="row">
+	<div class="col-md-12">
+		<!-- general form elements -->
+		<div class="box box-primary">
+			<div class="box-header with-border">
+				<h3 class="box-title">Type Property Form</h3>
+			</div>
+			<!-- /.box-header -->
+			<!-- form start -->
+			<form class="form-horizontal" role="form">
+				<div class="box-body">
+					<div class="form-group">
+						<label for="inputEmail3" class="col-sm-2 control-label">Name</label>
+						<div class="col-sm-10">
+						<input type="text" class="form-control" placeholder="name">
+						</div>
+					</div>
+				</div>
+				<!-- /.box-body -->
+			</form>
+		</div>
+		<!-- /.box -->
+	</div>
+</div>
