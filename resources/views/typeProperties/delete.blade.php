655c6aac0ac26982b6d56c770f766552316a06be
ajpadilla <ajpadilla88@gmail.com>
2017-02-19 22:44:46 -0400

    Agregando formulario para eliminar item tipo de propiedad

---
+++ resources/views/typeProperties/delete.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.modals.delete')
+
+@section('modal-delete-form-title')
+	Form Delete Title
+@stop
+
+@section('modal-delete-form-content')
+	Form Delete content
+	{{--@include('admin.schools.fields')--}}
+@stop
