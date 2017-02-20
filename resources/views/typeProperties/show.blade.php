c33d058fb11cf047a33820991dac16f20fb991f1
ajpadilla <ajpadilla88@gmail.com>
2017-02-19 22:45:17 -0400

    Agregando formulario para mostrar item tipo de propiedad

---
+++ resources/views/typeProperties/show.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.modals.show')
+
+@section('modal-show-form-title')
+	Form Show Title
+@stop
+
+@section('modal-show-form-content')
+	Form Show content
+	{{--@include('admin.schools.fields')--}}
+@stop
