06ef8a82199b75b3807773b5f30260c84bf9b31e
ajpadilla <ajpadilla88@gmail.com>
2017-02-19 22:45:05 -0400

    Agregando formulario para crear/editar item tipo de propiedad

---
+++ resources/views/typeProperties/form.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.modals.form')
+
+@section('modal-form-title')
+	Form (Create-Edit) Title
+@stop
+
+@section('modal-form-content')
+	Form (Create-Edit) content
+	{{--@include('admin.schools.fields')--}}
+@stop
