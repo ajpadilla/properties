2352a798748b9f894d0e5ad23764491e33a7069b
ajpadilla <ajpadilla88@gmail.com>
2017-02-20 17:09:51 -0400

    Agregando blade para mostrar errores

---
+++ resources/views/layouts/flash.blade.php
@@ -0,0 +1,5 @@
+<div v-if="" class="alert">
+	<b>@{{ flashMessage }}</b>
+	{{--<hr v-if="$validation.errors">
+	<validator-errors :component="'custom-error'" :validation="$validation"></validator-errors>--}}
+</div>
