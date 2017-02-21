a941317ae61093b0a1b6f6f612b6a40157e25075
ajpadilla <ajpadilla88@gmail.com>
2017-02-20 23:42:08 -0400

    Agregando metodo store al controlador

--- app/Http/Controllers/TypePropertyController.php
+++ app/Http/Controllers/TypePropertyController.php
@@ -4,11 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\TypeProperty;
+use App\Http\Requests\CreateTypePropertyRequest;
 
 class TypePropertyController extends Controller
 {
+
     public function index(Request $request)
     {
     	return response()->json(TypeProperty::paginate(2));
+    }
+
+    public function store(CreateTypePropertyRequest $request)
+    {
+    	if ($request->ajax()) {
+   			$input = $request->all();
+    		TypeProperty::create($input);
+    		$this->setSuccess(true);
+    		$this->addToResponseArray('data', $input);
+    		return $this->getResponseArrayJson();
+    	}
+    	return $this->getResponseArrayJson();
     }
 }
