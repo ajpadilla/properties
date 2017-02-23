3688f78016632a21e7f248f81b0a0e2ff3966040
ajpadilla <ajpadilla88@gmail.com>
2017-02-22 22:31:52 -0400

    Actualizando controlador

--- app/Http/Controllers/TypePropertyController.php
+++ app/Http/Controllers/TypePropertyController.php
@@ -5,24 +5,62 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\TypeProperty;
 use App\Http\Requests\CreateTypePropertyRequest;
+use App\Http\Requests\UpdateTypePropertyRequest;
 
 class TypePropertyController extends Controller
 {
 
     public function index(Request $request)
     {
-    	return response()->json(TypeProperty::paginate(2));
+    	return response()->json(TypeProperty::paginate(10));
     }
 
     public function store(CreateTypePropertyRequest $request)
     {
     	if ($request->ajax()) {
    			$input = $request->all();
-    		TypeProperty::create($input);
+    		$typeProperty = TypeProperty::create($input);
     		$this->setSuccess(true);
-    		$this->addToResponseArray('data', $input);
+    		$this->addToResponseArray('data', $typeProperty);
+            $this->addToResponseArray('message', 'Type of property successfully added');
     		return $this->getResponseArrayJson();
     	}
     	return $this->getResponseArrayJson();
+    }
+
+    public function show(Request $request, $id)
+    {
+        if ($request->ajax()) {
+            $typeProperty = TypeProperty::find($id);
+            $this->setSuccess(true);
+            $this->addToResponseArray('message', 'Type of property successfully recovered');
+            $this->addToResponseArray('data', $typeProperty);
+            return $this->getResponseArrayJson();
+        }
+        return $this->getResponseArrayJson();
+    }
+
+    public function update(UpdateTypePropertyRequest $request, $id)
+    {
+        if ($request->ajax()) {
+            $input = $request->all();
+            $typeProperty = TypeProperty::find($id);
+            $typeProperty->update($input);
+            $this->setSuccess(true);
+            $this->addToResponseArray('data', $typeProperty);
+            return $this->getResponseArrayJson();
+        }
+        return $this->getResponseArrayJson();
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        if ($request->ajax()) {
+            $typeProperty = TypeProperty::find($id);
+            $this->setSuccess($typeProperty->delete());
+            $this->addToResponseArray('message', 'School delete');
+            return $this->getResponseArrayJson(); 
+        }
+        return $this->getResponseArrayJson(); 
     }
 }
