c6b7bc8c6c4e991094dacaad2c75a27f3088181b
ajpadilla <ajpadilla88@gmail.com>
2017-02-22 22:34:03 -0400

    Actualizando blade

--- resources/views/layouts/modals/show.blade.php
+++ resources/views/layouts/modals/show.blade.php
@@ -1,9 +1,24 @@
 <modal v-if="showModal" @close='closeModal("showModal")'>
 	<div slot="header">
-		  <b>@yield('modal-show-form-title')</b>
+		<b>@yield('modal-show-form-title')</b>
+		@include('layouts.flash')
 	</div>	
 	<div slot="body">
-		  <b>@yield('modal-show-form-content')</b>
+		<b>@yield('modal-show-form-content')</b>
+		<div class="row">
+			<div class="col-sm-offset-2 col-sm-8">
+				<table class="table-responsive">
+					<tbody>
+						<template v-for="(field, value) in row">		
+							<tr v-if="">
+								<th style="width:50%">@{{ field }}</th>
+								<td>@{{ value }}</td>
+							</tr>
+						</template>
+					</tbody>
+				</table>
+			</div>
+		</div>
 	</div>	
 	<template slot="footer">
 		{{--<button type="button" class="btn btn-default" @click='closeModal("showModal")'>Close</button>--}}
