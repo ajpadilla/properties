8fd921bddfa0ca65ba5da5f6fcf16c958e66bed8
ajpadilla <ajpadilla88@gmail.com>
2017-02-22 22:33:13 -0400

    Mostrando mensajes de respuesta del servidor

--- resources/views/layouts/flash.blade.php
+++ resources/views/layouts/flash.blade.php
@@ -1,5 +1,12 @@
-<div v-if="" class="alert">
+<div v-if="flashMessage" :class="{ alert: true, 'alert-success': flashType == 'success',  
+'alert-danger': flashType == 'danger'}">
 	<b>@{{ flashMessage }}</b>
+
 	{{--<hr v-if="$validation.errors">
 	<validator-errors :component="'custom-error'" :validation="$validation"></validator-errors>--}}
+</div>
+<div class="row">
+	<ul class="list-group" v-if="errorMessages">
+	  <li class="list-group-item" v-for="error in errorMessages">@{{ error }}</li>
+	</ul>
 </div>
