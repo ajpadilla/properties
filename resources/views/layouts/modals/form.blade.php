13c173db8e45300ac859408d8d1a83ed5425c6aa
ajpadilla <ajpadilla88@gmail.com>
2017-02-22 22:33:51 -0400

    Actualizando blade

--- resources/views/layouts/modals/form.blade.php
+++ resources/views/layouts/modals/form.blade.php
@@ -8,8 +8,6 @@
 	</template>	
 	<template slot="footer">
 		{{--<button type="button" class="btn btn-default" @click='closeModal("formModal")'>Close</button>--}}
-		<button type="button" class="btn btn-success" @click='submit' v-if="">
-		Save
-		</button>
+		<button type="button" class="btn btn-success" @click='submit'>Save</button>
 	</template>	
 </modal>
