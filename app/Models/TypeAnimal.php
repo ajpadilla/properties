b0e16c4f5b928892d735040ab9ffe8ef6fa30000
ajpadilla <ajpadilla88@gmail.com>
2017-02-27 15:11:48 -0400

    Agregando modelo para tabla tipos de animales

--- app/Models/TypeAnimal.php
+++ app/Models/TypeAnimal.php
@@ -1,10 +1,26 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\SearchTrait;
+use App\Models\SortTrait;
 
 class TypeAnimal extends Model
 {
-    //
+ 	use SearchTrait, SortTrait;
+
+	public $table = 'type_properties';
+
+	/**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+
+    protected $searchableColumns = [
+    	'name'
+    ];
+
 }
