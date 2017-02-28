61b53cff0efd20146b711b85473e84597e5e8241
ajpadilla <ajpadilla88@gmail.com>
2017-02-28 18:02:06 -0400

    Modificando titulo del index

--- resources/views/disabilities/index.blade.php
+++ resources/views/disabilities/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div id="app">
     <section class="content-header">
-        <h1 class="pull-left">Listado tipos de animales</h1>
+        <h1 class="pull-left">Listado de discapacidades</h1>
         <h1 class="pull-right">
            <a class="btn btn-primary pull-right" href="#" style="margin-top: -10px;margin-bottom: 5px" @click.prevent="modal('POST')">Create</a>
        </h1>
@@ -29,7 +29,7 @@
 {{--@section('title-content', 'Listing event types')--}}
 
 @push('vue-scripts')
-    {!! Html::script('js/models/typeAnimal/config.js') !!}
+    {!! Html::script('js/models/disability/config.js') !!}
     <script type="text/javascript">
         var token = '{{ csrf_token() }}';
         var fieldInitOrder = 'id';
