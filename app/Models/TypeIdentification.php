dd9488f6958115fb5d0fcf764ae06b682e87d19b
ajpadilla <ajpadilla88@gmail.com>
2017-02-28 17:57:50 -0400

    Agregando modelo para tabla types_identifications

---
+++ app/Models/TypeIdentification.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\SearchTrait;
+use App\Models\SortTrait;
+
+class TypeIdentification extends Model
+{
+   	use SearchTrait, SortTrait;
+
+	public $table = 'types_identifications';
+
+	/**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+
+    /**
+     * Attributes that are for searchers of the model.
+     *
+     * @var array
+     */
+    protected $searchableColumns = [
+    	'name'
+    ];
+}
