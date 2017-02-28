646ac518ab4db97ec9929780c29d166370fb72dd
ajpadilla <ajpadilla88@gmail.com>
2017-02-28 16:25:39 -0400

    Agregando clase DisabilityTableSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(TypePropertyTableSeeder::class);
         $this->call(TypeAnimalTableSeeder::class);
+        $this->call(DisabilityTableSeeder::class);
     }
 }
