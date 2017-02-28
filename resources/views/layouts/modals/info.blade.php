8f239b69091740eba0842e5e11ed197bb04d054f
ajpadilla <ajpadilla88@gmail.com>
2017-02-28 15:45:41 -0400

    Modificando elementos del blade

--- resources/views/layouts/modals/info.blade.php
+++ resources/views/layouts/modals/info.blade.php
@@ -2,8 +2,14 @@
 	<div slot="header">
 	</div>	
 	<template slot="body">
-		@include('layouts.flash')
-	</template>	
-	<template slot="footer">
-	</template>	
+		<div v-if="flashSuccessfulMessage" :class="{ alert: true, 'alert-success': flashSuccessfulType == 'success',  
+		'alert-danger': flashSuccessfulType == 'danger'}">
+		<b>@{{ flashSuccessfulMessage }}</b>
+
+		{{--<hr v-if="$validation.errors">
+		<validator-errors :component="'custom-error'" :validation="$validation"></validator-errors>--}}
+		</div>
+</template>	
+<template slot="footer">
+</template>	
 </modal>
