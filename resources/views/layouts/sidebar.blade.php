d3b250bb293b6249d2e69b9999109bccaa95742c
ajpadilla <ajpadilla88@gmail.com>
2017-02-28 16:01:00 -0400

    Agregando rutas para modelos tipos de animales y tipos de propiedades

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -26,16 +26,33 @@
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu">
         <li class="header">MAIN NAVIGATION</li>
-        <li class="active treeview">
+
+        <li class="active">
+          <a href="{{ url('/admin/dashboard') }}">
+            <i class="fa fa-dashboard"></i> 
+            <span>Dashboard</span>
+          </a>
+        </li>
+
+        <li class="treeview">
           <a href="#">
-            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+            <i class="fa fa-pencil-square-o"></i> 
+            <span>Datos</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
           </a>
           <ul class="treeview-menu">
-            <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
-            <li><a href="index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+            <li class="">
+              <a href="{{ url('admin/typeProperties') }}">
+                <i class="fa fa-circle-o"></i> Tipos de propiedades
+              </a>
+            </li>
+            <li>
+              <a href="{{ url('admin/typeAnimals') }}">
+                <i class="fa fa-circle-o"></i> Tipos de animales
+              </a>
+            </li>
           </ul>
         </li>
         <li class="treeview">
