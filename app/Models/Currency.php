5304aa395661e08a2e7933cf771e1ce72c025d22
ajpadilla <ajpadilla88@gmail.com>
2017-03-01 00:16:08 -0400

    Definiendo modelo Currency

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Currency extends Model
 {
-    //
+	use SearchTrait, SortTrait;
+
+	public $table = 'currencies';
+
+	/**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+
+    
+    /**
+     * Attributes that are for searchers of the model.
+     *
+     * @var array
+     */
+    protected $searchableColumns = [
+    	'name'
+    ];
 }
