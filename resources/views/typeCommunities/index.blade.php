8e681dec5cbeb7071f1acceb11bcdc427e1ac6fc
ajpadilla <ajpadilla88@gmail.com>
2017-02-28 22:58:40 -0400

    Agregando blades para modals para crud del modelo EducationalLevel

--- resources/views/typeCommunities/index.blade.php
+++ resources/views/typeCommunities/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div id="app">
     <section class="content-header">
-        <h1 class="pull-left">Listado tipos de animales</h1>
+        <h1 class="pull-left">Listado tipos de comunidades</h1>
         <h1 class="pull-right">
            <a class="btn btn-primary pull-right" href="#" style="margin-top: -10px;margin-bottom: 5px" @click.prevent="modal('POST')">Create</a>
        </h1>
