8f5c104f3e1683b0935dbf7188ada5ecbb31e929
ajpadilla <ajpadilla88@gmail.com>
2017-03-02 00:19:48 -0400

    Agregando modelo para tabla types_infractions

---
+++ app/Models/TypeInfraction.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\SearchTrait;
+use App\Models\SortTrait;
+
+class TypeInfraction extends Model
+{
+
+	use SearchTrait, SortTrait;
+
+	public $table = 'types_infractions';
+
+	/**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+
+    /**
+     * Attributes that are for searchers of the model.
+     *
+     * @var array
+     */
+    protected $searchableColumns = [
+    	'name'
+    ];
+
+}
