1a96145cbe82b3f1e0819b2de2ded864333d518e
ajpadilla <ajpadilla88@gmail.com>
2017-03-09 02:30:42 -0400

    Agregando ruta para el archivo de configuración del modelo state

--- webpack.mix.js
+++ webpack.mix.js
@@ -52,6 +52,7 @@ mix.copy('resources/assets/js/models/typePqr/config.js', 'public/js/models/typeP
 mix.copy('resources/assets/js/models/typeRepresentative/config.js', 'public/js/models/typeRepresentative/config.js');
 mix.copy('resources/assets/js/models/typeInfraction/config.js', 'public/js/models/typeInfraction/config.js');
 mix.copy('resources/assets/js/models/country/config.js', 'public/js/models/country/config.js');
+mix.copy('resources/assets/js/models/state/config.js', 'public/js/models/state/config.js');
 
 
 mix.js('resources/assets/js/crud.js', 'public/js/crud.js');
