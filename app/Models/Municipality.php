0ea608e8e01e162a4bed4d4ad67c75ce39f814bc
ajpadilla <ajpadilla88@gmail.com>
2017-03-10 16:06:08 -0400

    Definiendo modelo para tabla municipalities

---
+++ app/Models/Municipality.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\State;
+use App\Models\SearchTrait;
+use App\Models\SortTrait;
+
+class Municipality extends Model
+{
+
+	use SearchTrait, SortTrait;
+
+	public $table = 'municipalities';
+
+	/**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['code', 'name', 'state_id'];
+
+    
+    /**
+     * Attributes that are for searchers of the model.
+     *
+     * @var array
+     */
+    protected $searchableColumns = [
+        'code',
+    	'name',
+        'state' => [
+            'table' => 'states',
+            'name'
+        ]
+    ];
+
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+    */
+
+    protected $appends = [
+        'state_name'
+    ];
+
+    /**
+     * ------ Relations ------ 
+    */
+
+    public function state()
+    {
+        return $this->belongsTo(State::class);
+    }
+
+    /**
+     *
+     *-------------------- Accessors and Mutators
+     *
+    */
+
+    public function getStateNameAttribute()
+    {
+        if($this->state)
+            return $this->state->name;
+        return false;
+    }
+
+}
