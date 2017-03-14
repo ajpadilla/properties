9e704a46b00bbdc5d062f719a4bb910f12c5a31f
ajpadilla <ajpadilla88@gmail.com>
2017-03-14 19:12:00 -0400

    Agregando archivo de configuración

---
+++ public/js/models/municipality/config.js
@@ -0,0 +1,69 @@
+var mapVar = 'inputMaterialsOptions'; 
+
+var objectRow = {            
+    id: "",
+    code: "",
+    name: "",
+    state_id: "",
+    created_at: "",
+    updated_at: "",
+    state: {
+        id: "",
+        name: "",
+        code: "",
+        area_code: "",
+        country_id: "",
+        created_at: "",
+        updated_at: ""
+    }
+};
+
+var modals = {
+    state_ADD_inform: false,
+};
+
+var tableColumns = [
+    {
+        name: 'id',
+        sortField: 'id',
+        visible: false
+    },
+    {
+        name: 'code',
+        title: 'Código',
+        sortField: 'code',
+        visible: true
+    },
+    {
+        name: 'name',
+        title: 'Nombre',
+        sortField: 'name',
+        visible: true
+    },
+    {
+        name: 'state_name',
+        title: 'País',
+        sortField: 'state.name',
+        visible: true
+    },
+    {
+        name: 'created_at',
+        title: 'Creado en',
+        sortField: 'created_at',
+        visible: false
+    },
+    {
+        name: 'updated_at',
+        title: 'Actualizado en',
+        sortField: 'updated_at',
+        visible: false
+    },       
+    {
+        name: '__component:custom-actions',
+        title: 'Actions',
+        titleClass: 'text-center',
+        dataClass: 'text-center'
+    },
+];
+
+
