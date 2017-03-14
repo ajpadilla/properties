8d74f237128236a116829c827626f59a8ae67e82
ajpadilla <ajpadilla88@gmail.com>
2017-03-14 19:36:36 -0400

    Creando migración para tabla communities

---
+++ database/migrations/2017_03_14_231723_create_communities_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommunitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('communities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('nit');
+            $table->string('name', 45);
+            $table->string('home_phone', 20);
+            $table->string('auxiliary_phone', 20)->nullable();
+            $table->string('cell_phone', 20)->nullable();
+            $table->string('auxiliary_cell', 20)->nullable();
+            $table->string('home_email', 30);
+            $table->string('auxiliary_email', 30)->nullable();
+            $table->text('address');
+            $table->boolean('status');
+            $table->dateTime('opening_date');
+            $table->dateTime('cancellation_date');
+            $table->text('reason_retiring');
+            $table->integer('municipality_id')->unsigned();
+            $table->foreign('municipality_id')
+              ->references('id')
+              ->on('municipalities')
+              ->onUpdate('cascade')
+              ->onDelete('cascade');
+            $table->integer('type_community_id')->unsigned();
+            $table->foreign('type_community_id')
+              ->references('id')
+              ->on('types_communities')
+              ->onUpdate('cascade')
+              ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('communities');
+    }
+}
