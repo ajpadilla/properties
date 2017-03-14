f289e2a20436ca911d36a51c796f25010fbbe2b5
ajpadilla <ajpadilla88@gmail.com>
2017-03-14 19:11:16 -0400

    Agregando request para crear/editar items de model Municipality

---
+++ app/Http/Requests/UpdateMunicipalityRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateMunicipalityRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'code' => 'min:1|max:15|unique:municipalities,code,'.$this->id,
+            'name' => 'required|min:1|max:30|unique:municipalities,name,'.$this->id,
+            'state_id' => 'required|integer|exists:states,id'
+        ];
+    }
+}
