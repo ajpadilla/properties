692e057dacff72c5f8a8ae365dedad2ec710588e
ajpadilla <ajpadilla88@gmail.com>
2017-03-20 16:12:47 -0400

    Agregando blades para el modelo Community

---
+++ resources/views/communities/fields.blade.php
@@ -0,0 +1,109 @@
+<div class="row">
+	<div class="col-md-12">
+		<!-- general form elements -->
+		<div class="box box-primary">
+			<div class="box-header with-border">
+				<h3 class="box-title">Community Form</h3>
+			</div>
+			<!-- /.box-header -->
+			<!-- form start -->
+			<form class="form-horizontal" role="form">
+				<div class="box-body">
+					<div class="col-sm-6">
+						<div class="form-group">
+							<label for="nit" class="col-sm-2 control-label">Nit</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.nit" class="form-control" placeholder="nit">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="name" class="col-sm-2 control-label">Name</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.name" class="form-control" placeholder="name">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="home_phone" class="col-sm-2 control-label">Home phone</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.home_phone" class="form-control" placeholder="home_phone">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="auxiliary_phone" class="col-sm-2 control-label">Auxiliary phone</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.auxiliary_phone" class="form-control" placeholder="auxiliary_phone">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="cell_phone" class="col-sm-2 control-label">Cell phone</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.cell_phone" class="form-control" placeholder="cell_phone">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="cell_phone" class="col-sm-2 control-label">Estatus</label>
+							<div class="col-sm-10">
+								<input type="radio" id="active" value="Active" v-model="row.status">
+								<label for="active">Activa</label>
+								<br>
+								<input type="radio" id="inactive" value="Inactive" v-model="row.status">
+								<label for="inactive">Inactiva</label>
+								<br>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="opening_date" class="col-sm-2 control-label">Ingreso</label>
+							<div class="col-sm-10">
+								<datepicker v-model="row.opening_date" format="yyyy MM dd" name="uniquename">
+								</datepicker>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="reason_retiring" class="col-sm-2 control-label">Reason retiring</label>
+							<div class="col-sm-10">
+								<textarea class="form-control" rows="1" v-model="row.reason_retiring">
+								</textarea>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="form-group">
+							<label for="auxiliary_cell" class="col-sm-2 control-label">Auxiliary cell</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.auxiliary_cell" class="form-control" placeholder="auxiliary_cell">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="home_email" class="col-sm-2 control-label">Home email</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.home_email" class="form-control" placeholder="home_email">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="auxiliary_email" class="col-sm-2 control-label">Auxiliary email</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.auxiliary_email" class="form-control" placeholder="auxiliary_email">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="address" class="col-sm-2 control-label">Address</label>
+							<div class="col-sm-10">
+								<textarea class="form-control" rows="1" v-model="row.address">
+								</textarea>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="cancellation_date" class="col-sm-2 control-label">Salida</label>
+							<div class="col-sm-10">
+								<datepicker v-model="row.cancellation_date" format="yyyy MM dd" name="uniquename">
+								</datepicker>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- /.box-body -->
+			</form>
+		</div>
+		<!-- /.box -->
+	</div>
+</div>
---
+++ resources/views/communities/index.blade.php
@@ -0,0 +1,79 @@
+@extends('layouts.dashboard')
+
+@section('content')
+<div id="app">
+    <section class="content-header">
+        <h1 class="pull-left">Listado de países</h1>
+        <h1 class="pull-right">
+           <a class="btn btn-primary pull-right" href="#" style="margin-top: -10px;margin-bottom: 5px" @click.prevent="modal('POST')">Create</a>
+       </h1>
+    </section>
+   <div class="content" style="padding-top: 30px;">
+        <div class="box box-primary">
+            <div class="box-body">
+                {{-- Include Table --}}
+                @include('communities.table')
+            </div>
+        </div>
+    </div>
+
+    {{-- Modals --}}
+    @include('communities.form')
+    @include('communities.show')
+    @include('communities.delete') 
+    @include('layouts.modals.info')
+
+    {{-- Relation Modals --}}
+
+
+    <pre>@{{ $data }}</pre>
+</div>
+@endsection
+
+{{--@section('title-content', 'Listing event types')--}}
+
+@push('vue-scripts')
+    {!! Html::script('js/models/community/config.js') !!}
+    <script type="text/javascript">
+        var token = '{{ csrf_token() }}';
+        var fieldInitOrder = 'id';
+        var apiUrl = {
+            store: "{{ route('api.countries.store') }}/",
+            update: "{{ route('api.countries.update') }}/",
+            show: "{{ route('api.countries.show') }}/",
+            delete: "{{ route('api.countries.delete') }}/",
+            foreign: {
+                currency: {
+                    select: {
+                        method: 'GET',
+                        url: "{{ route('api.v1.currencies.select-list') }}"
+                    },
+                    store: {
+                        method: 'POST',
+                        url: "{{ route('api.currencies.store') }}"
+                    }
+                }
+            }
+        };
+    </script>
+    {!! Html::script('js/crud.js') !!}
+    <script type="text/javascript">
+        var vm = window.vm;
+
+        var loadCurrencies = function () {
+            vm.getForeignData(vm.url.foreign.currency.select.url, 'currencyOptions', 'currency', 'select');
+        };
+
+        loadCurrencies();
+
+        /**
+         * Load currecies list after add new currency from add new item form
+        */
+        vm.$watch('localModals.currency_ADD_inform', function (value) {
+            if ( !value ) {
+                loadCurrencies();
+            }
+        });
+
+    </script>
+@endpush
