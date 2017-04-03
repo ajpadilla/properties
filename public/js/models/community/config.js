5b8831828b94af602730ce9ae7262178a9dfe1fe
ajpadilla <ajpadilla88@gmail.com>
2017-04-03 14:40:01 -0400

    Actualizando archivo

--- public/js/models/community/config.js
+++ public/js/models/community/config.js
@@ -39,71 +39,77 @@ var modals = {
 };
 
 var tableColumns = [
-    {
-        name: 'id',
-        sortField: 'id',
-        visible: false
-    },
-    {
-        name: 'nit',
-        title: 'Nit',
-        sortField: 'nit',
-        visible: true
-    },
-    {
-        name: 'name',
-        title: 'Nombre',
-        sortField: 'name',
-        visible: true
-    },
-    {
-        name: 'status_format',
-        title: 'Estatus',
-        sortField: 'status',
-        visible: true
-    },
-    {
-        name: 'municipality_name',
-        title: 'Municipio',
-        sortField: 'municipality.name',
-        visible: true
-    },
-    {
-        name: 'type_community_name',
-        title: 'Tipo de comunidad',
-        sortField: 'type_community.name',
-        visible: true
-    },
-    {
-        name: 'opening_date',
-        title: 'Ingreso',
-        sortField: 'opening_date',
-        visible: true
-    },
-    {
-        name: 'cancellation_date',
-        title: 'Salida',
-        sortField: 'cancellation_date',
-        visible: true
-    },
-    {
-        name: 'created_at',
-        title: 'Creado en',
-        sortField: 'created_at',
-        visible: false
-    },
-    {
-        name: 'updated_at',
-        title: 'Actualizado en',
-        sortField: 'updated_at',
-        visible: false
-    },       
-    {
-        name: '__component:custom-actions',
-        title: 'Actions',
-        titleClass: 'text-center',
-        dataClass: 'text-center'
-    },
+{
+    name: 'id',
+    sortField: 'id',
+    visible: false
+},
+{
+    name: 'nit',
+    title: 'Nit',
+    sortField: 'nit',
+    visible: true
+},
+{
+    name: 'name',
+    title: 'Nombre',
+    sortField: 'name',
+    visible: true
+},
+{
+    name: 'status_format',
+    title: 'Estatus',
+    sortField: 'status',
+    visible: true
+},
+{
+    name: 'municipality_name',
+    title: 'Municipio',
+    sortField: 'municipality.name',
+    visible: true
+},
+{
+    name: 'type_community_name',
+    title: 'Tipo de comunidad',
+    sortField: 'type_community.name',
+    visible: true
+},
+{
+    name: 'opening_date',
+    title: 'Ingreso',
+    sortField: 'opening_date',
+    visible: true
+},
+{
+    name: 'cancellation_date',
+    title: 'Salida',
+    sortField: 'cancellation_date',
+    visible: true
+},
+{
+    name: 'created_at',
+    title: 'Creado en',
+    sortField: 'created_at',
+    visible: false
+},
+{
+    name: 'updated_at',
+    title: 'Actualizado en',
+    sortField: 'updated_at',
+    visible: false
+},       
+{
+    name: '__component:custom-actions',
+    title: 'Actions',
+    titleClass: 'text-center',
+    dataClass: 'text-center'
+},
+{
+  name: '__slot:actions',
+  title: 'Actions',
+  titleClass: 'center aligned',
+  dataClass: 'center aligned'
+}  
 ];
 
 
