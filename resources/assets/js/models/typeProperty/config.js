44662c60e85b26a2b590b97fd6e5dbed61361350
ajpadilla <ajpadilla88@gmail.com>
2017-04-03 14:41:33 -0400

    Organizando código

--- resources/assets/js/models/typeProperty/config.js
+++ resources/assets/js/models/typeProperty/config.js
@@ -8,40 +8,40 @@ var objectRow = {
 };
 
 var tableColumns = [
-    {
-        name: 'id',
-        sortField: 'id',
-        visible: false
-    },
-    {
-        name: 'name',
-        title: 'Nombre',
-        sortField: 'name',
-        visible: true
-    },
-    {
-        name: 'created_at',
-        title: 'Creado en',
-        sortField: 'created_at',
-        visible: false
-    },
-    {
-        name: 'updated_at',
-        title: 'Actualizado en',
-        sortField: 'updated_at',
-        visible: false
-    },       
-    {
-        name: '__component:custom-actions',
-        title: 'Actions',
-        titleClass: 'text-center',
-        dataClass: 'text-center'
-    },
-       {
-          name: '__slot:actions',
-          title: 'Actions',
-          titleClass: 'center aligned',
-          dataClass: 'center aligned'
-        }       
+{
+    name: 'id',
+    sortField: 'id',
+    visible: false
+},
+{
+    name: 'name',
+    title: 'Nombre',
+    sortField: 'name',
+    visible: true
+},
+{
+    name: 'created_at',
+    title: 'Creado en',
+    sortField: 'created_at',
+    visible: false
+},
+{
+    name: 'updated_at',
+    title: 'Actualizado en',
+    sortField: 'updated_at',
+    visible: false
+},       
+{
+    name: '__component:custom-actions',
+    title: 'Actions',
+    titleClass: 'text-center',
+    dataClass: 'text-center'
+},
+{
+  name: '__slot:actions',
+  title: 'Actions',
+  titleClass: 'center aligned',
+  dataClass: 'center aligned'
+}       
 ];
 
