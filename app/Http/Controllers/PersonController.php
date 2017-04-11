e2da6f1e399b88b0018b6f0e3476c27fa282b465
ajpadilla <ajpadilla88@gmail.com>
2017-04-11 11:55:44 -0400

    Definiendo controlador con metodos list, index

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Person;
 
 class PersonController extends Controller
 {
-    //
+
+	public function index(Request $request)
+	{
+		if ($request->has('sort')) {
+			list($sortCol, $sortDir) = explode('|', $request->sort);
+			if (\Schema::hasColumn('persons', $sortCol)) {
+				$query = Person::orderBy($sortCol, $sortDir);
+			}else{
+				$query = Person::sortBy($sortCol, $sortDir);
+			}
+		}else{
+			$query = Person::orderBy('created_at', 'asc');
+		}
+
+		if($request->exists('filter')){
+			$query->search("{$request->filter}");
+		}
+
+		$perPage = $request->has('per_page') ? (int) $request->per_page : null;
+		$result = $query->paginate($perPage);
+
+		return response()->json($result);
+	}
+
+	public function list()
+    {
+        return view('persons.index');
+    }
+
+
 }
