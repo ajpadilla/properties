5757137acd7e9a549192e396f49b51677d50418d
ajpadilla <ajpadilla88@gmail.com>
2017-04-11 11:56:47 -0400

    Definiendo blades principales para el crud del modelo Person

---
+++ resources/views/persons/fields.blade.php
@@ -0,0 +1,54 @@
+<div class="row">
+	<div class="col-md-12">
+		<!-- general form elements -->
+		<div class="box box-primary">
+			<div class="box-header with-border">
+				<h3 class="box-title">Estate Form</h3>
+			</div>
+			<!-- /.box-header -->
+			<!-- form start -->
+			<form class="form-horizontal" role="form">
+				<div class="box-body">
+					<div class="form-group">
+						<label for="inputEmail3" class="col-sm-2 control-label">Code</label>
+						<div class="col-sm-10">
+							<input type="text" v-model="row.code" class="form-control" placeholder="code">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="inputEmail3" class="col-sm-2 control-label">Name</label>
+						<div class="col-sm-10">
+							<input type="text" v-model="row.name" class="form-control" placeholder="name">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="inputEmail3" class="col-sm-2 control-label">Area code</label>
+						<div class="col-sm-10">
+							<input type="text" v-model="row.area_code" class="form-control" placeholder="area code">
+						</div>
+					</div>
+					<!-- Country Id Field -->
+					<div class="form-group">
+						<label for="country_id" class="col-sm-2 control-label">Country:</label>
+						<div class="col-sm-10">
+							{{--<div class="input-group">--}}
+								<basic-select :options="foreignData.countryOptions"
+									:selected-option="row.country_related"
+									placeholder="select item"
+									@select="onSelect">
+								</basic-select>
+							{{--<span class="input-group-btn">
+								<button class="btn btn-primary" @click.prevent="modal('country_ADD_inform')">
+									<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+								</button>
+							</span>--}}
+							{{--</div>--}}
+						</div>
+					</div>
+			</div>
+			<!-- /.box-body -->
+		</form>
+	</div>
+	<!-- /.box -->
+</div>
+</div>
