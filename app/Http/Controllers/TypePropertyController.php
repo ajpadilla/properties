49d2871b494f7f0b40a6b6e92f93be403d6c4e8d
ajpadilla <ajpadilla88@gmail.com>
2017-04-11 23:38:24 -0400

    Actualizando metodo list

--- app/Http/Controllers/TypePropertyController.php
+++ app/Http/Controllers/TypePropertyController.php
@@ -34,7 +34,7 @@ class TypePropertyController extends Controller
 
     }
 
-    public function list()
+    public function list(Request $request)
     {
         return view('typeProperties.index');
     }
