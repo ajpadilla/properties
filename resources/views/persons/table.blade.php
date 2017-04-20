ef2ade6fccdc47245f9aabc278db8e522d15c165
ajpadilla <ajpadilla88@gmail.com>
2017-04-20 15:30:32 -0400

    Agregando sección para manipular imagenes en cada item de tipo person

--- resources/views/persons/table.blade.php
+++ resources/views/persons/table.blade.php
@@ -11,6 +11,21 @@
   @vuetable:cell-clicked="onCellClicked"
   @vuetable:pagination-data="onPaginationData"
   >
+  <template slot="actions" scope="props">
+    <!-- Single button -->
+    <div class="btn-group">
+      <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Action <span class="caret"></span>
+      </button>
+      <ul class="dropdown-menu">
+        <li>
+          <a href="#"  @click.prevent="slotAction('photo_ADD_inform', props.rowData, '{{ route('api.persons.addPhoto') }}/')">
+            <i class="glyphicon glyphicon-plus"></i>Add Photo
+          </a>
+        </li>
+      </ul>
+    </div>
+  </template>
 </vuetable> 
 
 <div class="vuetable-pagination">
