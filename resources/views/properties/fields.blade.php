c0011cec45ad24461a8d99fc7aea5f8a702c0dac
ajpadilla <ajpadilla88@gmail.com>
2017-04-20 23:14:40 -0400

    Agregando blades para crud del modelo property

---
+++ resources/views/properties/fields.blade.php
@@ -0,0 +1,282 @@
+<div class="row">
+	<div class="col-md-12">
+		<!-- general form elements -->
+		<div class="box box-primary">
+			<div class="box-header with-border">
+				<h3 class="box-title">Person Form</h3>
+			</div>
+			<!-- /.box-header -->
+			<!-- form start -->
+			<form class="form-horizontal" role="form">
+				<div class="box-body">
+					<div class="col-sm-6">
+						<div class="form-group">
+							<label for="inputEmail3" class="col-sm-2 control-label">Identification number</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.identification_number" class="form-control" placeholder="Code">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="inputEmail3" class="col-sm-2 control-label">Business name</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.business_name" class="form-control" placeholder="Business name">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="inputEmail3" class="col-sm-2 control-label">First name</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.first_name" class="form-control" placeholder="First name">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="inputEmail3" class="col-sm-2 control-label">Second name</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.second_name" class="form-control" placeholder="Second name">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="inputEmail3" class="col-sm-2 control-label">First surname</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.first_surname" class="form-control" placeholder="First name">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="inputEmail3" class="col-sm-2 control-label">Second surname</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.second_surname" class="form-control" placeholder="Second name">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="inputEmail3" class="col-sm-2 control-label">Home phone</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.home_phone" class="form-control" placeholder="Home phone">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="inputEmail3" class="col-sm-2 control-label">Auxiliary phone</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.auxiliary_phone" class="form-control" placeholder="Auxiliary phone">
+							</div>
+						</div>	
+						<div class="form-group">
+							<label for="inputEmail3" class="col-sm-2 control-label">Cell phone</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.cell_phone" class="form-control" placeholder="Cell phone">
+							</div>
+						</div>	
+						<div class="form-group">
+							<label for="inputEmail3" class="col-sm-2 control-label">Auxiliary cell</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.auxiliary_cell" class="form-control" placeholder="Auxiliary cell">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="inputEmail3" class="col-sm-2 control-label">Home email</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.home_email" class="form-control" placeholder="Home email">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="inputEmail3" class="col-sm-2 control-label">Auxiliary email</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.auxiliary_email" class="form-control" placeholder="Auxiliary email">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="reason_retiring" class="col-sm-2 control-label">Correspondence address</label>
+							<div class="col-sm-10">
+								<textarea class="form-control" rows="1" v-model="row.correspondence_address">
+								</textarea>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="inputEmail3" class="col-sm-2 control-label">City correspondence</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.city_correspondence" class="form-control" placeholder="City correspondence">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="inputEmail3" class="col-sm-2 control-label">Country correspondence</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.country_correspondence" class="form-control" placeholder="Country correspondence">
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="form-group">
+							<label for="reason_retiring" class="col-sm-2 control-label">Office address</label>
+							<div class="col-sm-10">
+								<textarea class="form-control" rows="1" v-model="row.office_address">
+								</textarea>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="inputEmail3" class="col-sm-2 control-label">City office</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.city_office" class="form-control" placeholder="City office">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="inputEmail3" class="col-sm-2 control-label">Country office</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.country_office" class="form-control" placeholder="Country office">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="inputEmail3" class="col-sm-2 control-label">Birth date</label>
+							<div class="col-sm-10">
+								{{--<input type="date" v-model="row.birth_date" class="form-control" placeholder="Birth date">--}}
+								<datepicker v-model="row.birth_date" format="yyyy-MM-dd">
+								</datepicker>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="cell_phone" class="col-sm-2 control-label">Gender</label>
+							<div class="col-sm-10">
+								<input type="radio" id="male" value="male" v-model="row.gender">
+								<label for="male">Male</label>
+								<br>
+								<input type="radio" id="female" value="female" v-model="row.gender">
+								<label for="female">Female</label>
+								<br>
+							</div>
+						</div>
+						<!-- civil status Id Field -->
+						<div class="form-group">
+							<label for="currency_id" class="col-sm-2 control-label">Civil status:</label>
+							<div class="col-sm-10">
+								{{--<basic-select :options=""
+									:selected-option="row.country_related"
+									placeholder="select item"
+									@select="onSelect">
+								</basic-select>--}}
+								<select v-model="row.civil_status">
+								  <option disabled value="">Please select one</option>
+								  <option>married</option>
+								  <option>bachelor</option>
+								  <option>divorced</option>
+								</select>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="inputEmail3" class="col-sm-2 control-label">Cod labor activity</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.cod_labor_activity" class="form-control" placeholder="Cod labor activity">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="inputEmail3" class="col-sm-2 control-label">Admission date</label>
+							<div class="col-sm-10">
+								{{--<input type="date" v-model="row.admission_date" class="form-control" placeholder="Admission date">--}}
+								<datepicker v-model="row.admission_date" format="yyyy-MM-dd">
+								</datepicker>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="inputEmail3" class="col-sm-2 control-label">Cancellation date</label>
+							<div class="col-sm-10">
+								{{--<input type="date" v-model="row.cancellation_date" class="form-control" placeholder="Cancellation date">--}}
+								<datepicker v-model="row.cancellation_date" format="yyyy-MM-dd">
+								</datepicker>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="cell_phone" class="col-sm-2 control-label">Status</label>
+							<div class="col-sm-10">
+								<input type="radio" id="active" value="1" v-model="row.status">
+								<label for="active">Activa</label>
+								<br>
+								<input type="radio" id="inactive" value="0" v-model="row.status">
+								<label for="inactive">Inactiva</label>
+								<br>
+							</div>
+						</div>
+						<!-- Contry Id Field -->
+						<div class="form-group">
+							<label for="currency_id" class="col-sm-2 control-label">Country:</label>
+							<div class="col-sm-10">
+								<basic-select :options="foreignData.countryOptions"
+									:selected-option="row.country_related"
+									placeholder="select item"
+									@select="onSelect">
+								</basic-select>
+								{{--<select class="form-control" v-model="row.country_related.value">
+									<option v-for="country in foreignData.countryOptions" 
+									v-bind:value="country.value">		
+										@{{ country.text }}
+									</option>
+								</select>--}}
+							</div>
+						</div>
+						<!-- State Id Field -->
+						<div class="form-group">
+							<label for="currency_id" class="col-sm-2 control-label">State:</label>
+							<div class="col-sm-10">
+								<basic-select :options="foreignData.stateOptions"
+									:selected-option="row.state_related"
+									placeholder="select item"
+									@select="onSelect">
+								</basic-select>
+								{{--<select class="form-control" v-model="row.state_related.value">
+									<option v-for="state in foreignData.stateOptions" 
+									v-bind:value="state.value">		
+										@{{ state.text}}
+									</option>
+								</select>--}}
+							</div>
+						</div>
+						<!-- City birth Id Field -->
+						<div class="form-group">
+							<label for="currency_id" class="col-sm-2 control-label">City birth</label>
+							<div class="col-sm-10">
+								<basic-select :options="foreignData.cityBirthOptions"
+									:selected-option="row.city_birth_related"
+									placeholder="select item"
+									@select="onSelect">
+								</basic-select>
+							</div>
+						</div>
+						<!-- Disability Id Field -->
+						<div class="form-group">
+							<label for="currency_id" class="col-sm-2 control-label">Disability</label>
+							<div class="col-sm-10">
+								<basic-select :options="foreignData.disabilityOptions"
+									:selected-option="row.disability_related"
+									placeholder="select item"
+									@select="onSelect">
+								</basic-select>
+							</div>
+						</div>
+
+						<!-- educational level Id Field -->
+						<div class="form-group">
+							<label for="currency_id" class="col-sm-2 control-label">Educational Level</label>
+							<div class="col-sm-10">
+								<basic-select :options="foreignData.educationalLevelOptions"
+									:selected-option="row.educational_level_related"
+									placeholder="select item"
+									@select="onSelect">
+								</basic-select>
+							</div>
+						</div>
+
+						<!-- Type identification Id Field -->
+						<div class="form-group">
+							<label for="currency_id" class="col-sm-2 control-label">Type identification</label>
+							<div class="col-sm-10">
+								<basic-select :options="foreignData.typeIdentificationOptions"
+									:selected-option="row.type_identification_related"
+									placeholder="select item"
+									@select="onSelect">
+								</basic-select>
+							</div>
+						</div>
+					</div>
+					
+				</div>
+				<!-- /.box-body -->
+			</form>
+		</div>
+		<!-- /.box -->
+	</div>
+</div>
---
+++ resources/views/properties/index.blade.php
@@ -0,0 +1,135 @@
+@extends('layouts.dashboard')
+
+@section('content')
+<div id="app">
+    <section class="content-header">
+        <h1 class="pull-left">Listado de propiedades</h1>
+        <h1 class="pull-right">
+           <a class="btn btn-primary pull-right" href="#" style="margin-top: -10px;margin-bottom: 5px" @click.prevent="modal('POST')">Create</a>
+       </h1>
+    </section>
+   <div class="content" style="padding-top: 30px;">
+        <div class="box box-primary">
+            <div class="box-body">
+                {{-- Include Table --}}
+                @include('properties.table')
+            </div>
+        </div>
+    </div>
+
+    {{-- Modals --}}
+    @include('properties.form')
+    @include('properties.show')
+    @include('properties.delete') 
+    @include('layouts.modals.info')
+
+    {{-- Relation Modals --}}
+    {{--@include('persons.photos.add')--}}
+
+    <pre>@{{ $data }}</pre>
+</div>
+@endsection
+
+{{--@section('title-content', 'Listing event types')--}}
+
+@push('vue-scripts')
+    {!! Html::script('js/models/person/config.js') !!}
+    <script type="text/javascript">
+        var token = '{{ csrf_token() }}';
+        var fieldInitOrder = 'id';
+        var apiUrl = {
+            store: "{{ route('api.persons.store') }}/",
+            show: "{{ route('api.persons.show') }}/",
+            update: "{{ route('api.persons.update') }}/",
+            delete: "{{ route('api.persons.delete') }}/",
+            foreign: {
+               country: {
+                select: {
+                    method: 'GET',
+                    url: "{{ route('api.v1.countries.select-list') }}/"
+                }
+            },
+            state: {
+                byCountry: {
+                    method: 'GET',
+                    url: "{{ route('api.v1.states.byCountry') }}/"
+                }
+            },
+            {{-- Municipality --}}
+            city_birth:{
+                select: {
+                    method: 'GET',
+                    url: "{{ route('api.v1.municipalities.select-list') }}/"
+                },
+                byState: {
+                    method: 'GET',
+                    url: "{{ route('api.v1.municipalities.byState') }}/"
+                }
+            },
+            disability:{
+                select: {
+                    method: 'GET',
+                    url: "{{ route('api.v1.disabilities.select-list') }}/"
+                },
+            },
+            educational_level:{
+                select: {
+                    method: 'GET',
+                    url: "{{ route('api.v1.educationalLevels.select-list') }}/"
+                },
+            },
+            type_identification:{
+                select: {
+                    method: 'GET',
+                    url: "{{ route('api.v1.typeIdentifications.select-list') }}/"
+                },
+            },
+        }
+    };
+    </script>
+    {!! Html::script('js/crud.js') !!}
+    <script type="text/javascript">
+        var vm = window.vm;
+
+        var loadCountries = function () {
+            vm.getForeignData(vm.url.foreign.country.select.url, 'countryOptions', 'country', 'select');
+        };
+
+        var loadStates = function () {
+            console.log(vm.url.foreign.state.byCountry.url + vm.row.country_related.value);
+            vm.getForeignData(vm.url.foreign.state.byCountry.url + vm.row.country_related.value, 'stateOptions', 'state', 'byCountry');
+        };
+
+        var loadMunicipalities = function () {
+            vm.getForeignData(vm.url.foreign.city_birth.byState.url + vm.row.state_related.value, 'cityBirthOptions', 'city_birth', 'byState');
+        };
+
+        var loadDisability = function () {
+            vm.getForeignData(vm.url.foreign.disability.select.url, 'disabilityOptions', 'disability', 'select');
+            console.log();
+        }
+
+        var loadEducationalLevel = function () {
+            vm.getForeignData(vm.url.foreign.educational_level.select.url, 'educationalLevelOptions', 'educational_level', 'select');
+        }
+
+        var loadTypeIdentification = function () {
+            vm.getForeignData(vm.url.foreign.type_identification.select.url, 'typeIdentificationOptions', 'type_identification', 'select');
+        }
+
+
+        loadCountries();
+        loadDisability();
+        loadEducationalLevel();
+        loadTypeIdentification();
+
+        vm.$watch('row.country_related.value', function (value) {
+            loadStates();
+        });
+
+         vm.$watch('row.state_related.value', function (value) {
+            loadMunicipalities();
+        });
+
+    </script>
+@endpush
