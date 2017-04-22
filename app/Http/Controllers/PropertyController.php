759921c726ade176df12fc0e30216b08589061d7
ajpadilla <ajpadilla88@gmail.com>
2017-04-22 16:07:06 -0400

    Definiendo metodos para crud

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Property;
 use App\Http\Requests\CreatePropertyRequest;
+use App\Http\Requests\UpdatePropertyRequest;
 
 class PropertyController extends Controller
 {
-    
+
 
 	public function index(Request $request)
 	{
@@ -34,26 +35,70 @@ class PropertyController extends Controller
 	}
 
 	public function showList()
+	{
+		return view('properties.index');
+	}
+
+	public function store(CreatePropertyRequest $request)
+	{
+		if ($request->ajax()) {
+			$input = $request->all();
+			$input['type_property_id'] = $request->input('type_property_related.value');
+			unset($input['type_property_related']);
+			$input['community_id'] = $request->input('community_related.value');
+			unset($input['community_related']);
+			$input['person_id'] = $request->input('person_related.value');
+			unset($input['person_related']);
+			$property = Property::create($input);
+			$this->setSuccess(true);
+			$this->addToResponseArray('data', $property);
+			$this->addToResponseArray('message', 'Property successfully added');
+			return $this->getResponseArrayJson();
+		}
+		return $this->getResponseArrayJson();
+	}
+
+	public function show(Request $request, $id)
     {
-        return view('properties.index');
+    	if ($request->ajax()) {
+            $property = Property::find($id);
+            $this->setSuccess(true);
+            $this->addToResponseArray('message', 'Property successfully recovered');
+            $this->addToResponseArray('data', $property);
+            return $this->getResponseArrayJson();
+        }
+        return $this->getResponseArrayJson();   
     }
 
-    public function store(CreatePropertyRequest $request){
-    	if ($request->ajax()) {
-    		$input = $request->all();
-    		$input['type_property_id'] = $request->input('type_property_related.value');
-    		unset($input['type_property_related']);
-    		$input['community_id'] = $request->input('community_related.value');
-    		unset($input['community_related']);
-    		$input['person_id'] = $request->input('person_related.value');
-    		unset($input['person_related']);
-    		$property = Property::create($input);
-    		$this->setSuccess(true);
-    		$this->addToResponseArray('data', $property);
-    		$this->addToResponseArray('message', 'Property successfully added');
-    		return $this->getResponseArrayJson();
-    	}
-    	return $this->getResponseArrayJson();
+    public function update(UpdatePropertyRequest $request, $id)
+    {
+   		if ($request->ajax()) {
+			$input = $request->all();
+			$input['type_property_id'] = $request->input('type_property_related.value');
+			unset($input['type_property_related']);
+			$input['community_id'] = $request->input('community_related.value');
+			unset($input['community_related']);
+			$input['person_id'] = $request->input('person_related.value');
+			unset($input['person_related']);
+			$property = Property::find($id);
+			$property->update($input);
+			$this->setSuccess(true);
+			$this->addToResponseArray('data', $property);
+			$this->addToResponseArray('message', 'Property successfully update');
+			return $this->getResponseArrayJson();
+		}
+		return $this->getResponseArrayJson();
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        if ($request->ajax()) {
+            $property = Property::find($id);
+            $this->setSuccess($property->delete());
+            $this->addToResponseArray('message', 'Property successfully delete');
+            return $this->getResponseArrayJson(); 
+        }
+        return $this->getResponseArrayJson(); 
     }
 
 
