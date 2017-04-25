a64697a9f319fefae5d80422389aa1aed2858f36
ajpadilla <ajpadilla88@gmail.com>
2017-04-25 16:52:32 -0400

    Agregando migración para modelo Cartera

--- database/migrations/2017_04_24_194614_create_briefcases_table.php
+++ database/migrations/2017_04_24_194614_create_briefcases_table.php
@@ -15,6 +15,17 @@ class CreateBriefcasesTable extends Migration
     {
         Schema::create('briefcases', function (Blueprint $table) {
             $table->increments('id');
+            $table->date('date_cut');
+            $table->date('publication_date');
+            $table->string('house_number', 20);
+            $table->string('block', 20);
+            $table->string('area', 20);
+            $table->decimal('management_fee', 5,2);
+            $table->decimal('interest_on_arrears', 5,2);
+            $table->decimal('interest_current', 5,2);
+            $table->decimal('parking_fee', 5,2);
+            $table->decimal('motorcycle_parking_fee', 5,2);
+            $table->decimal('sanction_assembly', 5,2);
             $table->timestamps();
         });
     }
