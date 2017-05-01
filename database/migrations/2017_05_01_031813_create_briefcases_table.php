99927fe56c3c2e3977bf6e30a34486a573f5375f
ajpadilla <ajpadilla88@gmail.com>
2017-04-30 23:22:12 -0400

    Rehaciendo tabla para cartera de morosos del proyecto

--- database/migrations/2017_05_01_031813_create_briefcases_table.php
+++ database/migrations/2017_05_01_031813_create_briefcases_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBriefcaseTable extends Migration
+class CreateBriefcasesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateBriefcaseTable extends Migration
      */
     public function up()
     {
-        Schema::create('briefcase', function (Blueprint $table) {
+        Schema::create('briefcases', function (Blueprint $table) {
             $table->increments('id');
             $table->date('date_cut');
             $table->date('publication_date');
@@ -44,6 +44,6 @@ class CreateBriefcaseTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('briefcase');
+        Schema::dropIfExists('briefcases');
     }
 }
