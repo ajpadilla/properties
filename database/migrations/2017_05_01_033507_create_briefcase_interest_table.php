017d9cc30401629c4bc24fccf5ed9d4e61cd0a9e
ajpadilla <ajpadilla88@gmail.com>
2017-04-30 23:41:36 -0400

    Agregando migración para manipular relación entre cartera de morosos y tabla de intereses

---
+++ database/migrations/2017_05_01_033507_create_briefcase_interest_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBriefcaseInterestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('briefcase_interest', function (Blueprint $table) {
+            $table->increments('id');
+            $table->float('percent', 8, 2);  
+            $table->integer('briefcase_id')->unsigned();
+            $table->foreign('briefcase_id')
+            ->references('id')
+            ->on('briefcases')
+            ->onDelete('cascade');
+            $table->integer('interest_id')->unsigned();
+            $table->foreign('interest_id')
+            ->references('id')
+            ->on('interests')
+            ->onDelete('restrict')
+            ->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('briefcase_interest');
+    }
+}
