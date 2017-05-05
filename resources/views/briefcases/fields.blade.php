1c765218072c090d545f1abd391bfe5a93ac36b5
ajpadilla <ajpadilla88@gmail.com>
2017-05-05 15:50:00 -0400

    Agregando campos para el modelo Briefcase

--- resources/views/briefcases/fields.blade.php
+++ resources/views/briefcases/fields.blade.php
@@ -3,155 +3,133 @@
 		<!-- general form elements -->
 		<div class="box box-primary">
 			<div class="box-header with-border">
-				<h3 class="box-title">Community Form</h3>
+				<h3 class="box-title">Briefcase Form</h3>
 			</div>
 			<!-- /.box-header -->
 			<!-- form start -->
 			<form class="form-horizontal" role="form">
 				<div class="box-body">
 					<div class="col-sm-6">
+						
 						<div class="form-group">
-							<label for="nit" class="col-sm-2 control-label">Nit</label>
+							<label for="date_cut" class="col-sm-2 control-label">Date cut</label>
 							<div class="col-sm-10">
-								<input type="text" v-model="row.nit" class="form-control" placeholder="nit">
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="name" class="col-sm-2 control-label">Name</label>
-							<div class="col-sm-10">
-								<input type="text" v-model="row.name" class="form-control" placeholder="name">
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="home_phone" class="col-sm-2 control-label">Home phone</label>
-							<div class="col-sm-10">
-								<input type="text" v-model="row.home_phone" class="form-control" placeholder="home_phone">
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="auxiliary_phone" class="col-sm-2 control-label">Auxiliary phone</label>
-							<div class="col-sm-10">
-								<input type="text" v-model="row.auxiliary_phone" class="form-control" placeholder="auxiliary_phone">
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="cell_phone" class="col-sm-2 control-label">Cell phone</label>
-							<div class="col-sm-10">
-								<input type="text" v-model="row.cell_phone" class="form-control" placeholder="cell_phone">
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="cell_phone" class="col-sm-2 control-label">Estatus</label>
-							<div class="col-sm-10">
-								<input type="radio" id="active" value="1" v-model="row.status">
-								<label for="active">Activa</label>
-								<br>
-								<input type="radio" id="inactive" value="0" v-model="row.status">
-								<label for="inactive">Inactiva</label>
-								<br>
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="opening_date" class="col-sm-2 control-label">Ingreso</label>
-							<div class="col-sm-10">
-								<datepicker v-model="row.opening_date" format="yyyy-MM-dd">
+								<datepicker v-model="row.date_cut" format="yyyy-MM-dd">
 								</datepicker>
 								{{--<input type="text" v-model="row.opening_date" class="form-control" placeholder="opening_date">--}}
 							</div>
 						</div>
+							
 						<div class="form-group">
-							<label for="reason_retiring" class="col-sm-2 control-label">Reason retiring</label>
+							<label for="publication_date" class="col-sm-2 control-label">Publication date</label>
 							<div class="col-sm-10">
-								<textarea class="form-control" rows="1" v-model="row.reason_retiring">
-								</textarea>
+								<datepicker v-model="row.publication_date" format="yyyy-MM-dd">
+								</datepicker>
+								{{--<input type="text" v-model="row.opening_date" class="form-control" placeholder="opening_date">--}}
 							</div>
 						</div>
+
+						<div class="form-group">
+							<label for="honorarium" class="col-sm-2 control-label">Honorarium</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.honorarium" class="form-control" placeholder="honorarium">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label for="total_capital" class="col-sm-2 control-label">Total capital</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.total_capital" class="form-control" placeholder="total_capital">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label for="total_sanction" class="col-sm-2 control-label">Total sanction</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.total_sanction" class="form-control" placeholder="total_sanction">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label for="total_interest" class="col-sm-2 control-label">Total interest</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.total_interest" class="form-control" placeholder="total_interest">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label for="total_debt" class="col-sm-2 control-label">Total debt</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.total_debt" class="form-control" placeholder="total_debt">
+							</div>
+						</div>
+
 					</div>
 					<div class="col-sm-6">
+
 						<div class="form-group">
-							<label for="auxiliary_cell" class="col-sm-2 control-label">Auxiliary cell</label>
+							<label for="cell_phone" class="col-sm-2 control-label">Debt indicator</label>
 							<div class="col-sm-10">
-								<input type="text" v-model="row.auxiliary_cell" class="form-control" placeholder="auxiliary_cell">
+								<input type="radio" id="active" value="1" v-model="row.debt_indicator">
+								<label for="active">Si</label>
+								<br>
+								<input type="radio" id="inactive" value="0" v-model="row.debt_indicator">
+								<label for="inactive">No</label>
+								<br>
 							</div>
 						</div>
+
 						<div class="form-group">
-							<label for="home_email" class="col-sm-2 control-label">Home email</label>
+							<label for="cell_phone" class="col-sm-2 control-label">Sms indicator</label>
 							<div class="col-sm-10">
-								<input type="text" v-model="row.home_email" class="form-control" placeholder="home_email">
+								<input type="radio" id="si" value="1" v-model="row.sms_indicator">
+								<label for="si">Si</label>
+								<br>
+								<input type="radio" id="inactiveno" value="0" v-model="row.sms_indicator">
+								<label for="no">No</label>
+								<br>
 							</div>
 						</div>
+								
 						<div class="form-group">
-							<label for="auxiliary_email" class="col-sm-2 control-label">Auxiliary email</label>
+							<label for="positive_balance" class="col-sm-2 control-label">Positive balance</label>
 							<div class="col-sm-10">
-								<input type="text" v-model="row.auxiliary_email" class="form-control" placeholder="auxiliary_email">
+								<input type="text" v-model="row.positive_balance" class="form-control" placeholder="positive_balance">
 							</div>
 						</div>
+
 						<div class="form-group">
-							<label for="address" class="col-sm-2 control-label">Address</label>
+							<label for="application_code" class="col-sm-2 control-label">Application code</label>
 							<div class="col-sm-10">
-								<textarea class="form-control" rows="1" v-model="row.address">
-								</textarea>
+								<input type="text" v-model="row.application_code" class="form-control" placeholder="application_code">
 							</div>
 						</div>
+
 						<div class="form-group">
-							<label for="cancellation_date" class="col-sm-2 control-label">Salida</label>
+							<label for="debt_height" class="col-sm-2 control-label">Debt height</label>
 							<div class="col-sm-10">
-								<datepicker v-model="row.cancellation_date" format="yyyy-MM-dd">
-								</datepicker>
-								{{--<input type="text" v-model="row.cancellation_date" class="form-control" placeholder="cancellation_date">--}}
+								<input type="text" v-model="row.debt_height" class="form-control" placeholder="debt_height">
 							</div>
 						</div>
-						<!-- Contry Id Field -->
+
+						<!-- Community Id Field -->
 						<div class="form-group">
-							<label for="currency_id" class="col-sm-2 control-label">Country:</label>
+							<label for="currency_id" class="col-sm-2 control-label">Community:</label>
 							<div class="col-sm-10">
-								<basic-select :options="foreignData.countryOptions"
-									:selected-option="row.country_related"
-									placeholder="select item"
-									@select="onSelect">
-								</basic-select>
-								{{--<select class="form-control" v-model="row.country_related.value">
-									<option v-for="country in foreignData.countryOptions" 
-									v-bind:value="country.value">		
-										@{{ country.text }}
-									</option>
-								</select>--}}
-							</div>
-						</div>
-						<!-- State Id Field -->
-						<div class="form-group">
-							<label for="currency_id" class="col-sm-2 control-label">State:</label>
-							<div class="col-sm-10">
-								<basic-select :options="foreignData.stateOptions"
-									:selected-option="row.state_related"
-									placeholder="select item"
-									@select="onSelect">
-								</basic-select>
-								{{--<select class="form-control" v-model="row.state_related.value">
-									<option v-for="state in foreignData.stateOptions" 
-									v-bind:value="state.value">		
-										@{{ state.text}}
-									</option>
-								</select>--}}
-							</div>
-						</div>
-						<!-- Municipality Id Field -->
-						<div class="form-group">
-							<label for="currency_id" class="col-sm-2 control-label">Municipality:</label>
-							<div class="col-sm-10">
-								<basic-select :options="foreignData.municipalityOptions"
-									:selected-option="row.municipality_related"
+								<basic-select :options="foreignData.communityOptions"
+									:selected-option="row.community_related"
 									placeholder="select item"
 									@select="onSelect">
 								</basic-select>
 							</div>
 						</div>
-						<!-- Type Community Id Field -->
+
+						<!-- Property Id Field -->
 						<div class="form-group">
-							<label for="currency_id" class="col-sm-2 control-label">Type Community:</label>
+							<label for="currency_id" class="col-sm-2 control-label">Property:</label>
 							<div class="col-sm-10">
-								<basic-select :options="foreignData.typeCommunityOptions"
-									:selected-option="row.type_community_related"
+								<basic-select :options="foreignData.propertyOptions"
+									:selected-option="row.property_related"
 									placeholder="select item"
 									@select="onSelect">
 								</basic-select>
