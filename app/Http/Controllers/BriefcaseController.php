5c3bd30917e799afa43a9f6b226a21dca22c65e5
ajpadilla <ajpadilla88@gmail.com>
2017-05-05 15:47:03 -0400

    Agregando metodoso para crud del modelo Briefcase

--- app/Http/Controllers/BriefcaseController.php
+++ app/Http/Controllers/BriefcaseController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Briefcase;
+use App\Http\Requests\CreateBreafcaseRequest;
+use App\Http\Requests\UpdateBreafcaseRequest;
+
 
 class BriefcaseController extends Controller
 {
@@ -31,9 +34,63 @@ class BriefcaseController extends Controller
 		return response()->json($result);
 	}
 
-	public function list()
+	public function showList()
     {
         return view('briefcases.index');
+    }
+
+    public function store(CreateBreafcaseRequest $request)
+    {
+    	if ($request->ajax()) {
+			$input = $request->all();
+            $input['property_id'] = (int) $request->input('property_related.value');
+            unset($input['property_related']);
+            $briefcase = Briefcase::create($input);
+			$this->setSuccess(true);
+    		$this->addToResponseArray('data', $briefcase);
+            $this->addToResponseArray('message', 'Briefcase successfully added');
+    		return $this->getResponseArrayJson();
+		}
+    	return $this->getResponseArrayJson();
+    }
+
+    public function show(Request $request, $id)
+    {
+    	if ($request->ajax()) {
+            $briefcase = Briefcase::find($id);
+            $this->setSuccess(true);
+            $this->addToResponseArray('message', 'Briefcase successfully recovered');
+            $this->addToResponseArray('data', $briefcase);
+            return $this->getResponseArrayJson();
+        }
+        return $this->getResponseArrayJson();   
+    }
+
+    public function update(UpdateBreafcaseRequest $request, $id)
+    {
+        if ($request->ajax()) {
+            $input = $request->all();
+            $input['property_id'] = (int) $request->input('property_related.value');
+            unset($input['property_related']);
+            $briefcase = Briefcase::find($id);
+            $briefcase->update($input);
+            $this->setSuccess(true);
+            $this->addToResponseArray('data', $briefcase);
+            $this->addToResponseArray('message', 'Briefcase successfully update');
+            return $this->getResponseArrayJson();
+        }
+        return $this->getResponseArrayJson();
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        if ($request->ajax()) {
+            $briefcase = Briefcase::find($id);
+            $this->setSuccess($briefcase->delete());
+            $this->addToResponseArray('message', 'Briefcase successfully delete');
+            return $this->getResponseArrayJson(); 
+        }
+        return $this->getResponseArrayJson(); 
     }
 
 
