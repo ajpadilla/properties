3ba0764c69b81ad9bcb063bdc4ff201a1ef3a725
ajpadilla <ajpadilla88@gmail.com>
2017-05-05 10:27:53 -0400

    Definiendo controlador para modelo Briefcase junto con su metodo index

---
+++ app/Http/Controllers/BriefcaseController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Briefcase;
+
+class BriefcaseController extends Controller
+{
+ 
+ 	public function index(Request $request)
+	{
+		if ($request->has('sort')) {
+			list($sortCol, $sortDir) = explode('|', $request->sort);
+			if (\Schema::hasColumn('briefcases', $sortCol)) {
+				$query = Briefcase::orderBy($sortCol, $sortDir);
+			}else{
+				$query = Briefcase::sortBy($sortCol, $sortDir);
+			}
+		}else{
+			$query = Briefcase::orderBy('created_at', 'asc');
+		}
+
+		if($request->exists('filter')){
+			$query->search("{$request->filter}");
+		}
+
+		$perPage = $request->has('per_page') ? (int) $request->per_page : null;
+		$result = $query->paginate($perPage);
+
+		return response()->json($result);
+	}
+
+
+}
