c5546fdf0df8ee011ea6fef86807d1f12d30ec1b
ajpadilla <ajpadilla88@gmail.com>
2017-05-09 10:18:59 -0400

    Agregando modelo Interest

---
+++ app/Models/Interest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+use App\Models\SearchTrait;
+use App\Models\SortTrait;
+
+class Interest extends Model
+{
+	use SearchTrait, SortTrait;
+
+	public $table = 'interests';
+
+	/**
+	     * The attributes that are mass assignable.
+	     *
+	     * @var array
+	  */
+	protected $fillable = [
+		'name', 
+	];
+
+  	/**
+     * Attributes that are for searchers of the model.
+     *
+     * @var array
+     */
+  	protected $searchableColumns = [
+
+  	];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+    */
+
+    protected $appends = [
+
+    ];
+
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+    */
+
+    protected $dates = [
+
+    ];
+
+    /**
+     * ------ Relations ------ 
+    */
+
+
+    /**
+     *
+     * -------Accessors And Mutators------
+     *
+    */
+
+}
