3df55b7d6fb38dae8212e536a54c21d379c1fa8d
ajpadilla <ajpadilla88@gmail.com>
2017-05-12 12:31:59 -0400

    agregando migración pra tabla peticiones, quejas y recursos

---
+++ database/migrations/2017_05_12_162637_create_services_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('date_origin');
+            $table->boolean('status');
+            $table->text('description');
+            $table->date('date_response');
+            $table->text('description_response');
+            $table->integer('type_service_id')->unsigned();
+            $table->foreign('type_service_id')
+              ->references('id')
+              ->on('type_services')
+              ->onUpdate('cascade')
+              ->onDelete('cascade');
+            $table->integer('property_id')->unsigned();
+            $table->foreign('property_id')
+              ->references('id')
+              ->on('properties')
+              ->onUpdate('cascade')
+              ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+}
