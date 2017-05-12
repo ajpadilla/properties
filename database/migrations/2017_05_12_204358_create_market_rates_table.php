25b0a54ded0d97711abe3b59ba91add6bc7dc553
ajpadilla <ajpadilla88@gmail.com>
2017-05-12 16:50:23 -0400

    Agregando migración para tabla market_rates(cotizaciones)

---
+++ database/migrations/2017_05_12_204358_create_market_rates_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMarketRatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('market_rates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('description');
+            $table->float('amount' 8,2);
+            $table->date('date_quote');
+            $table->date('approval_date');
+            $table->boolean('status');
+            $table->integer('supplier_id')->unsigned();
+            $table->foreign('supplier_id')
+              ->references('id')
+              ->on('suppliers')
+              ->onUpdate('cascade')
+              ->onDelete('cascade');
+            $table->integer('community_id')->unsigned();
+            $table->foreign('community_id')
+              ->references('id')
+              ->on('communities')
+              ->onUpdate('cascade')
+              ->onDelete('cascade'
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('market_rates');
+    }
+}
