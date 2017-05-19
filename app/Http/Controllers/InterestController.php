409c44f3a410e6be51e61754427fb660c5694042
ajpadilla <ajpadilla88@gmail.com>
2017-05-19 16:40:57 -0400

    Definiendo controlaodor y agregando metodo SelectList dentro del controlador

---
+++ app/Http/Controllers/InterestController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Interest;
+
+class InterestController extends Controller
+{
+
+    public function selectList(Request $request)
+    {
+        if ($request->ajax())
+        {   
+            $this->setSuccess(true);
+            $this->addToResponseArray('data', 
+                Interest::all()->pluck('name', 'id')->toArray()
+            );
+            return $this->getResponseArrayJson(); 
+        }
+        return $this->getResponseArrayJson(); 
+    }
+
+}
