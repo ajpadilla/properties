5cb86afb90cc3d77fad3496f69b948c064d8e6ee
ajpadilla <ajpadilla88@gmail.com>
2017-05-19 23:57:08 -0400

    Agregando controlador para modelo Sanction e incluyendo metodo

--- app/Http/Controllers/SanctionController.php
+++ app/Http/Controllers/SanctionController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Sanction;
 
 class SanctionController extends Controller
 {
-    //
+  
+   public function selectList(Request $request)
+    {
+        if ($request->ajax())
+        {   
+            $this->setSuccess(true);
+            $this->addToResponseArray('data', 
+                Sanction::all()->pluck('name', 'id')->toArray()
+            );
+            return $this->getResponseArrayJson(); 
+        }
+        return $this->getResponseArrayJson(); 
+    }
+
 }
