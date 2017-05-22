4081dde3331203628f2706801641685ac3145de0
ajpadilla <ajpadilla88@gmail.com>
2017-05-22 13:02:05 -0400

    Agregando blades para crud de modelo Sanction

---
+++ resources/views/sanctions/fields.blade.php
@@ -0,0 +1,26 @@
+<div class="row">
+	<div class="col-md-12">
+		<!-- general form elements -->
+		<div class="box box-primary">
+			<div class="box-header with-border">
+				<h3 class="box-title">Sanction form</h3>
+			</div>
+			<!-- /.box-header -->
+			<!-- form start -->
+			<form class="form-horizontal" role="form">
+				<div class="box-body">
+					<div class="col-sm-6">
+						<div class="form-group">
+							<label for="name" class="col-sm-2 control-label">Name</label>
+							<div class="col-sm-10">
+								<input type="text" v-model="row.name" class="form-control" placeholder="name">
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- /.box-body -->
+			</form>
+		</div>
+		<!-- /.box -->
+	</div>
+</div>
---
+++ resources/views/sanctions/index.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.dashboard')
+
+@section('content')
+<div id="app">
+    <section class="content-header">
+        <h1 class="pull-left">Listado de sanciones</h1>
+        <h1 class="pull-right">
+           <a class="btn btn-primary pull-right" href="#" style="margin-top: -10px;margin-bottom: 5px" @click.prevent="modal('POST')">Create</a>
+       </h1>
+    </section>
+   <div class="content" style="padding-top: 30px;">
+        <div class="box box-primary">
+            <div class="box-body">
+                {{-- Include Table --}}
+                @include('sanctions.table')
+            </div>
+        </div>
+    </div>
+
+    {{-- Modals --}}
+    @include('sanctions.form')
+    @include('sanctions.show')
+    @include('sanctions.delete') 
+    @include('layouts.modals.info')
+
+    {{-- Relation Modals --}}
+
+    <pre>@{{ $data }}</pre>
+</div>
+@endsection
+
+{{--@section('title-content', 'Listing event types')--}}
+
+@push('vue-scripts')
+    {!! Html::script('js/models/interest/config.js') !!}
+    <script type="text/javascript">
+        var token = '{{ csrf_token() }}';
+        var fieldInitOrder = 'id';
+        var apiUrl = {
+            store: "{{ route('api.sanctions.store') }}/",
+            update: "{{ route('api.sanctions.update') }}/",
+            show: "{{ route('api.sanctions.show') }}/",
+            delete: "{{ route('api.sanctions.delete') }}/",
+            foreign: {
+            }
+        };
+    </script>
+    {!! Html::script('js/crud.js') !!}
+    <script type="text/javascript">
+        var vm = window.vm;
+    </script>
+@endpush
