90b9a6a4be8d8fde144915d3df29eca8d54fa7c1
ajpadilla <ajpadilla88@gmail.com>
2017-05-22 12:25:57 -0400

    Modificando titulo del formulario

--- resources/views/interests/fields.blade.php
+++ resources/views/interests/fields.blade.php
@@ -3,7 +3,7 @@
 		<!-- general form elements -->
 		<div class="box box-primary">
 			<div class="box-header with-border">
-				<h3 class="box-title">Community Form</h3>
+				<h3 class="box-title">Interest form</h3>
 			</div>
 			<!-- /.box-header -->
 			<!-- form start -->
