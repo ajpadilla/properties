7c284a3311e73d5939025633337a4404c32275db
ajpadilla <ajpadilla88@gmail.com>
2017-05-22 12:07:01 -0400

    Modificando ruta de blades para modelo interest

--- resources/views/interests/index.blade.php
+++ resources/views/interests/index.blade.php
@@ -32,7 +32,7 @@
 {{--@section('title-content', 'Listing event types')--}}
 
 @push('vue-scripts')
-    {!! Html::script('js/models/community/config.js') !!}
+    {!! Html::script('js/models/interest/config.js') !!}
     <script type="text/javascript">
         var token = '{{ csrf_token() }}';
         var fieldInitOrder = 'id';
