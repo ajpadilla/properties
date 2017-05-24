4c068bcb1cc89fe13e5af677f465382b4fd1a0a1
ajpadilla <ajpadilla88@gmail.com>
2017-05-24 15:46:44 -0400

    Agregando archivo de configuración para asociar item interest con modelo breafcase

---
+++ public/js/models/briefcase/interest/config.js
@@ -0,0 +1,54 @@
+var mapVar = 'inputMaterialsOptions'; 
+
+var objectRow = {            
+    id: "",
+    name: "",
+    pivot: {
+        interest_id: "",
+        briefcase_id: "",
+        percent: ""
+    },
+    created_at: "",
+    updated_at: ""
+};
+
+
+var tableColumns = [
+    {
+        name: 'id',
+        sortField: 'id',
+        visible: false
+    },
+    {
+        name: 'name',
+        title: 'Nombre',
+        sortField: 'name',
+        visible: true
+    },
+    {
+        name: 'pivot.percent',
+        title: 'Percent',
+        sortField: 'name',
+        visible: true
+    },
+    {
+        name: 'created_at',
+        title: 'Creado en',
+        sortField: 'created_at',
+        visible: false
+    },
+    {
+        name: 'updated_at',
+        title: 'Actualizado en',
+        sortField: 'updated_at',
+        visible: false
+    },       
+    {
+      name: '__slot:actions',
+      title: 'Actions',
+      titleClass: 'center aligned',
+      dataClass: 'center aligned'
+    }  
+];
+
+
