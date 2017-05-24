a7cff77c0b3b8e28eed3fdcc7db5d7edc9a09f52
ajpadilla <ajpadilla88@gmail.com>
2017-05-24 15:48:05 -0400

    Modificando archivo config.js u ruta en boton principal

--- resources/views/briefcases/interests/index.blade.php
+++ resources/views/briefcases/interests/index.blade.php
@@ -3,21 +3,25 @@
 @section('content')
 <div id="app">
     <section class="content-header">
-        <h1 class="pull-left">Listado de intereses para cartega de conjunto</h1>
+        <h1 class="pull-left">Listado de intereses para cartega de conjunto {{ $briefcase->id }}</h1>
         <h1 class="pull-right">
-           <a class="btn btn-primary pull-right" href="#" style="margin-top: -10px;margin-bottom: 5px" @click.prevent="modal('POST')">Create</a>
+           <a class="btn btn-primary pull-right" href="{{ route('briefcases') }}" style="margin-top: -10px;margin-bottom: 5px">Carteras</a>
        </h1>
     </section>
    <div class="content" style="padding-top: 30px;">
         <div class="box box-primary">
             <div class="box-body">
                 {{-- Include Table --}}
-                @include('briefcases.table')
+                @include('briefcases.interests.table')
             </div>
         </div>
     </div>
 
     {{-- Modals --}}
+    @include('briefcases.interests.form')
+    @include('briefcases.interests.show')
+    @include('briefcases.interests.delete')
+    @include('layouts.modals.info')
 
     {{-- Relation Modals --}}
     
@@ -29,13 +33,13 @@
 {{--@section('title-content', 'Listing event types')--}}
 
 @push('vue-scripts')
-    {!! Html::script('js/models/briefcase/config.js') !!}
+    {!! Html::script('js/models/briefcase/interest/config.js') !!}
     <script type="text/javascript">
         var token = '{{ csrf_token() }}';
         var fieldInitOrder = 'id';
         var apiUrl = {
             store: "{{ route('api.briefcases.store') }}/",
-            show: "{{ route('api.briefcases.show') }}/",
+            show: "{{ route('api.v1.briefcases.interests.show', $briefcase->id) }}/",
             update: "{{ route('api.briefcases.update') }}/",
             delete: "{{ route('api.briefcases.delete') }}/",
             foreign: {
