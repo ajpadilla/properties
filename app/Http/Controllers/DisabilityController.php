6c22ac11a41773725f77e30dec654179f9a6b921
ajpadilla <ajpadilla88@gmail.com>
2017-05-28 22:41:04 -0400

    Modificando nombre de metodo para listar items a showList

--- app/Http/Controllers/DisabilityController.php
+++ app/Http/Controllers/DisabilityController.php
@@ -32,7 +32,7 @@ class DisabilityController extends Controller
 		return response()->json($result);
 	}
 
-    public function list()
+    public function showList()
     {
         return view('disabilities.index');
     }
