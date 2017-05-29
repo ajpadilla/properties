472cf1065f5f9b18ead5aa134eb217d38d670cc3
ajpadilla <ajpadilla88@gmail.com>
2017-05-29 11:17:07 -0400

    Modificando titulo de formulario

--- resources/views/properties/fields.blade.php
+++ resources/views/properties/fields.blade.php
@@ -3,7 +3,7 @@
 		<!-- general form elements -->
 		<div class="box box-primary">
 			<div class="box-header with-border">
-				<h3 class="box-title">Person Form</h3>
+				<h3 class="box-title">Property Form</h3>
 			</div>
 			<!-- /.box-header -->
 			<!-- form start -->
